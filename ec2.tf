743f6e0d571e6e22abd338814d6f6398a5622186
shiva shankar <sivas1475@gmail.com>
2022-12-30 04:14:53 +0530

    JENKINS

--- ec2.tf
+++ ec2.tf
@@ -5,6 +5,7 @@ resource "aws_spot_instance_request" "spot" {
   vpc_security_group_ids = [aws_security_group.main.id]
   subnet_id              = var.SUBNET_IDS[0]
   wait_for_fulfillment   = true
+  iam_instance_profile   = var.IAM_POLICY_CREATE ? aws_iam_instance_profile.instance-profile.*.name[0] : null
 
   tags = {
     Name = "${var.COMPONENT}-${var.ENV}"
